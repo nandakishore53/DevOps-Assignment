47932f325ba8f6ea9927345e480a4278f049c524
nanda kishore juttuka <nandujuttuka@gmail.com>
2026-07-02 23:00:55 +0530

    Initial commit: Add Express server for DevOps Assignment

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = 3000;
+
+// Home Route
+app.get("/", (req, res) => {
+    res.json({
+        message: "Welcome to DevOps Assignment",
+        status: "Running Successfully"
+    });
+});
+
+// Health Check Route
+app.get("/health", (req, res) => {
+    res.json({
+        status: "Healthy",
+        uptime: process.uptime()
+    });
+});
+
+// About Route
+app.get("/about", (req, res) => {
+    res.json({
+        project: "AWS DevOps Assignment",
+        author: "Nanda Kishore"
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
